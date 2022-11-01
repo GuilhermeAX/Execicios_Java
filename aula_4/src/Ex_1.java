fff2a6668e3d4cfda0e2e8a64e012a88621bff61
Guilherme A. Silva <guilherme.salmeida@mercadolivre.com>
2022-11-01 13:01:57 -0300

    Ajustes em nomes de variaveis e código para se enquadrarem no padrão do exercício

--- aula_4/src/Ex_1.java
+++ aula_4/src/Ex_1.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Ex_1 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         //int[] vetor = new int[10];//Se quiser fazer com entrada de dados, descomente essa linha e comente a proxima.
         int[] vetor = {2, 5, 1, 3, 4, 9, 7, 8, 10, 6};
-        int num;
+        int numero;
 
         //Se quiser fazer a entrada de dados descomente o código a seguir
         /*for(int i = 0; i < vetor.length; i++){
@@ -13,15 +13,15 @@ public class Ex_1 {
             vetor[i] = in.nextInt();
         }*/
 
-        System.out.println("Digite um valor para procurar no vetor");
-        num = in.nextInt();
+        System.out.print("Digite um valor para procurar no vetor: ");
+        numero = scanner.nextInt();
 
-        for(int i = 0; i < vetor.length; i++){
-            if(vetor[i] == num){
-                System.out.println("O índice do número digitado é " + i);
-
-            } else if(i == vetor.length-1){
-                System.out.println("Número não encontrado no vetor");
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] == numero) {
+                System.out.print("O número " + numero + " está localizado na posição: " + i);
+                break;
+            } else if (i == vetor.length - 1) {
+                System.out.print("O número "+numero+" não foi licalizado!");
             }
         }
     }
